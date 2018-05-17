a5d360555610528b6797fd588647ea4151203ebb
constmoon <jiiw.kang@gmail.com>
2018-05-18 00:21:23 +0900

    Initial commit

---
+++ face_config.py
@@ -0,0 +1,34 @@
+from PIL import Image, ImageDraw
+from pprint import pprint
+import cognitive_face as CF
+
+KEY = '5d2f03e34f454a91ba2cce29492bbc60'  # Replace with a valid subscription key (keeping the quotes in place).
+CF.Key.set(KEY)
+
+BASE_URL = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0'  # Replace with your regional Base URL
+CF.BaseUrl.set(BASE_URL)
+
+# You can use this example JPG or replace the URL below with your own URL to a JPEG image.
+img_url = 'images/face1.jpg'
+faces = CF.face.detect(img_url, attributes= "emotion")    # emotion-recognition
+pprint(faces)    # print data from detected face
+
+# Convert width height to a point in a rectangle
+def getRectangle(faceDictionary):
+    rect = faceDictionary['faceRectangle']
+    left = rect['left']
+    top = rect['top']
+    bottom = left + rect['height']
+    right = top + rect['width']
+    return ((left, top), (bottom, right))
+
+# Open the image from the url
+img = Image.open(img_url)
+
+# For each face returned use the face rectangle and draw a red box.
+draw = ImageDraw.Draw(img)
+for face in faces:
+    draw.rectangle(getRectangle(face), outline='red')
+
+# Display the image
+img.show()
